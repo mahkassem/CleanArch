1b257a043a561c376eef451ad9273a95e75adb90
Mahmoud Kassem <mahmoud.ali.kassem@gmail.com>
2021-09-20 02:24:38 +0200

    Update Mvc for new Context UniversityDBContext

--- CleanArch/CleanArch.Infra.Data/Context/UniversityDBContext.cs
+++ CleanArch/CleanArch.Infra.Data/Context/UniversityDBContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CleanArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,13 @@ using System.Threading.Tasks;
 
 namespace CleanArch.Infra.Data.Context
 {
-    public class UniversityDBContext
+    public class UniversityDBContext : DbContext
     {
+        public UniversityDBContext(DbContextOptions options) : base(options)
+        {
 
+        }
+
+        public DbSet<Course> Courses { get; set; }
     }
 }
